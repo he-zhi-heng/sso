76077dfdc96693101f64d4a5f38d5fdebf68d713
ubuntu <1090637222@qq.com>
2023-05-07 21:18:39 +0800

    init

---
+++ sso-gateway/src/main/java/com/he/ssogateway/SsoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.he.ssogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoGatewayApplication.class, args);
+    }
+
+}
